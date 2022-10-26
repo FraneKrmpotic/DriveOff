eda5ab9eef928b246f6518bdb752aebff2fce9bf
Frane Krmpotić <franekrmpotic@gmail.com>
2022-10-26 17:29:15 +0200

    Initial commit

---
+++ src/main/java/hr/algebra/DriveOff/DriveOffApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.DriveOff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveOffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/algebra/DriveOff/DriveOffApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.DriveOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
